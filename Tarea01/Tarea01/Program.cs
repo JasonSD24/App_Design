c0703abaf570f206a20d70d910458e719fbba89a
Jason Solano <jasonsolano4@outlook.com>
2015-05-24 17:15:46 -0600

    Listo

---
+++ Tarea01/Tarea01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            fibonacci(56);
+            System.Console.ReadKey();
+        }
+
+        public static void fibonacci(int n)
+        {
+            int num = 1;
+            int temp = 0;
+            int resultado;
+
+            while (num < n){
+                Console.WriteLine(num);
+                resultado = num + temp;
+                temp = num;
+                num = resultado;
+            }
+        }
+    }
+}
