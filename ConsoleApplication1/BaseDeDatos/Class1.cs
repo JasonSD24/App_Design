17d9acac6be3e6a514eae54cfdc1ef5c7f5eb8cc
JasonSD24 <jasonsolano4@outlook.com>
2015-06-01 20:54:32 -0600

    Listo

---
+++ ConsoleApplication1/BaseDeDatos/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class Numeros
+    {      
+
+
+        private List<int> DataBases(List<int> Lista)
+        {
+            List<int> n = new List<int>();
+
+            
+            {
+                
+            }            
+        }
+    }
